ed23a4a88aaae03788a6b5f4c85e742cb146e437
Piers Powlesland <pierspowlesland@gmail.com>
2023-09-05 22:51:26 +0100

    Half way there

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let price = if quantity > 40 { 1 } else { 2 };
+    quantity * price
+}
 
 // Don't modify this function!
 #[test]
